b5b7d2b0051ef363f56ba06a7bf400d0fe67d266
Gergely Karolyi <gergelykarolyi@gmail.com>
2021-03-11 12:27:58 +0100

    a bit of refactoring

--- app/jobs/cast_matcher_job.rb
+++ app/jobs/cast_matcher_job.rb
@@ -6,7 +6,7 @@ class CastMatcherJob < ApplicationJob
     if query.count > 1 && (actor_ids = Tmdb.matching_cast(query)) && actor_ids.count > 1
       # if we have 2 movie inputs and several matching actors
       actor_ids.map { |actor_id| Result.create(json: Tmdb.get_actor_details(actor_id)) }
-               .each { |actor| broadcast_actor(actor) }
+      actor_ids.each { |actor| broadcast_actor(actor) }
     else
       # if there is only 1 movie input or no matching actors display preresults page
       Tmdb.get_actors(query.last.to_i).each { |actor| broadcast_actor(actor) }
@@ -15,19 +15,12 @@ class CastMatcherJob < ApplicationJob
 
   private
 
-  def render_response(partial, locals)
-    ApplicationController.renderer.render_to_string(
-      partial: partial,
-      locals: { query: @query }.merge(locals)
-    )
-  end
-
-  def broadcast(response)
-    ActionCable.server.broadcast("CastMatcher_result_#{@query.join('&')}", { response: response })
-  end
-
   def broadcast_actor(actor)
-    response = render_response("shared/cards/actor_card", { actor: actor })
-    broadcast(response)
+    BroadcastJob.perform_now(
+      { channel: "CastMatcher",
+        query: @query,
+        partial: "shared/cards/actor_card",
+        locals: { actor: actor } }
+    )
   end
 end
