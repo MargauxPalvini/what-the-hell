137fa5cee36981940dc62142205134157fdbe5bf
Margaux Palvini <margaux.palvini@gmail.com>
2021-03-29 12:31:30 +0200

    delete models

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root to: "searches#home"
 
-  resources :searches, only: :create
-  resources :results, only: :show
-
-  # Sidekiq Web UI, only for admins.
-  require "sidekiq/web"
-
+  
 end
