aa73ce9002486deae755380abac0700325f53dd9
Margaux Palvini <margaux.palvini@gmail.com>
2021-04-04 17:14:10 +0200

    facial rteco

--- app/javascript/plugins/face_recognition.js
+++ app/javascript/plugins/face_recognition.js
@@ -1,7 +1,9 @@
 export const initFaceRecognitionForm = ()=>{
   const form = document.querySelector('.form-face-recognition')
-  const fileField = form.querySelector('.file-input')
-  fileField.addEventListener('change', (event)=>{
-    form.submit()
-  })
+  if(form) {
+    const fileField = form.querySelector('.file-input')
+    fileField.addEventListener('change', (event)=>{
+      form.submit()
+    })
+  }
 }
