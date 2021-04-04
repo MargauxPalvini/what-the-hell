1f07291c2569eb5c5e600637824ad3d4f404f60e
Margaux Palvini <margaux.palvini@gmail.com>
2021-04-04 17:01:44 +0200

    fdace recognition form style

--- app/controllers/facial_recognition_controller.rb
+++ app/controllers/facial_recognition_controller.rb
@@ -6,19 +6,15 @@ class FacialRecognitionController < ApplicationController
   def search
     uploaded_file = params[:picture]
     cloudinary = upload_to_cloudinary uploaded_file.path()
-    puts "*"*50
     actor = FaceRecognition.call cloudinary["url"]
 
     if actor 
       actor = actor["actor"]
       tmdb_actor = Tmdb.actor_from_name actor
-      puts tmdb_actor
       redirect_to show_actor_path(id: tmdb_actor["id"])
     else
       nil
     end
-
-    puts "*"*50
   end
 
   private 
